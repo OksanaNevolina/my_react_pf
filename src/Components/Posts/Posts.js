6c0b2cff9041a8e088acbcb56c4c7f113523ad8f
OksanaNevolina <onevolina5gmail.com>
2023-07-31 10:43:02 +0300

    classComponentPost

---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,16 @@
+class Posts extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state={
+            userId:'',
+            id:'',
+            title:'',
+            body:''
+        }
+    }
+    componentDidMount() {
+        console.log('cdsd65656')
+    }
+
+}
